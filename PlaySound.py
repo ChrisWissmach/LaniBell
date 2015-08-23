17ad639eb5c253eeebbc21539e733ca61b886b99
ChrisWissmach <chriswissmach@gmail.com>
2015-08-22 21:48:26 -0400

    Update sound

--- PlaySound.py
+++ PlaySound.py
@@ -1,6 +1,7 @@
 import pygame
+
 pygame.mixer.init()
-pygame.mixer.music.load("siren.wav")
+pygame.mixer.music.load("herecomesthemoney.wav")
 pygame.mixer.music.play()
 while pygame.mixer.music.get_busy() == True:
     continue
