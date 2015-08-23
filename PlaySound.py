08d3559018ecd6f2a6c65651bef2f339cc842b1d
ChrisWissmach <chriswissmach@gmail.com>
2015-08-23 15:31:38 -0400

    Added lights

--- PlaySound.py
+++ PlaySound.py
@@ -1,8 +1,22 @@
 #hehehehhhehehehehhehehehe :)
 import pygame
+import RPi.GPIO as GPIO
+import time
 
-pygame.mixer.init()
-pygame.mixer.music.load("herecomesthemoney.wav")
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy() == True:
-    continue
+def money():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(7,GPIO.OUT)
+    GPIO.setup(15,GPIO.OUT)
+    GPIO.output(7,False)
+    GPIO.output(15,False)
+    pygame.mixer.init()
+    pygame.mixer.music.load("herecomesthemoney.wav")
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy() == True:
+        GPIO.output(15,False)
+        GPIO.output(7,True)
+        time.sleep(0.1)
+        GPIO.output(7,False)
+        GPIO.output(15,True)
+        time.sleep(0.1)
+    GPIO.cleanup()
